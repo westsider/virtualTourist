4ec9700722e3ae54c237224c543e943d3ffa6705
Warren Hansen <whansen1@me.com>
2016-10-20 11:12:45 -0700

    Collection View

--- virtualTourist/AppDelegate.swift
+++ virtualTourist/AppDelegate.swift
@@ -13,7 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    let themeColor = UIColor(red: 0.01, green: 0.41, blue: 0.22, alpha: 1.0)
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
---
+++ virtualTourist/FlickrPhotoCell.swift
@@ -0,0 +1,16 @@
+//
+//  FlickrPhotoCell.swift
+//  virtualTourist
+//
+//  Created by Warren Hansen on 10/19/16.
+//  Copyright © 2016 Warren Hansen. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+
+}
+
